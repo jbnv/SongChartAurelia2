bb508defdcdc80725065fb8e84085571c33f8eb4
Jay Bienvenu <github@bienv.com>
2016-10-18 11:22:56 -0500

    Implement with numeral code disabled.

---
+++ src/decimal.ts
@@ -0,0 +1,9 @@
+//import * as numeral from 'numeral';
+
+export class DecimalValueConverter {
+  toView(value,places) {
+    var exponent = Math.pow(10,places);
+    return Math.floor(parseFloat(value) * exponent) / exponent;
+    //return numeral(value).format('0.0000'.substring(0,places+2));
+  }
+}
