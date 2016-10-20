66cf03fe14fc105005f7513d2c220d82d0f7623a
Jay Bienvenu <github@bienv.com>
2016-10-20 16:20:49 -0500

    Fix history routing.

--- src/resources/data.ts
+++ src/resources/data.ts
@@ -4,6 +4,7 @@ import {History} from '../resources/history';
 export class Data extends ReactiveCollection {
   title = "";
   subtitle = "";
+  route = "";
   fetchRoute = "";
   fetchRouteFn = null;
   slug = "";
@@ -17,12 +18,19 @@ export class Data extends ReactiveCollection {
     if (this.fetchRouteFn) { fetchRoute = this.fetchRouteFn(this.parameters); }
     this.navModel.setTitle(this.title);
     this.setPath(fetchRoute,this.massage.bind(this));
-    (new History()).mark(fetchRoute,this.title).then();
+
+    // Determine exact route for history.
+    let route = this.route;
+    for (var key in this.parameters) {
+      route = route.replace(":"+key,this.parameters[key]);
+    }
+    (new History()).mark(route,this.title).then();
   }
 
   activate(parameters,routeConfig) {
     this.parameters = parameters;
     this.slug = parameters.slug;
+    this.route = routeConfig.route;
     this.navModel = routeConfig.navModel;
     return this.refresh();
   }
