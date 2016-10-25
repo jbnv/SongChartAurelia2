d3e405b1b78e8933e42d88bf5f46b9d7ad469f1d
Jay Bienvenu <github@bienv.com>
2016-10-25 09:15:20 -0500

    Fix aggregation.

--- src/resources/elements/year-scales.ts
+++ src/resources/elements/year-scales.ts
@@ -13,19 +13,44 @@ export class YearScales {
   toggleCountGraph() { this.showCountGraph = !this.showCountGraph; }
   toggleSaaGraph() { this.showSaaGraph = !this.showSaaGraph; }
 
-  activate(songs) {
+  _aggregateEra(collection) {
+    var maxCount = 1; // ensure that divisor is always greater than 0
+    var maxAA = 0.01;
 
-    this.decades = {};
-    this.years = {};
+    for (var slug in collection) {
+      var item = collection[slug];
+      if (item.count == 0) continue;
+      item.score /= item.count;
+      if (item.count > maxCount) maxCount = item.count;
+      if (item.score > maxAA) maxAA = item.score;
+    }
+
+    for (var slug in collection) {
+      var item = collection[slug];
+      item.r = (item.score/maxAA)/(item.count/maxCount);
+      item.leader = (item.r >= 1.2);
+      item.lagger = (item.r <= 0.8);
+    }
+
+    return collection;
+
+  }
+
+  activate(songs) {
+    console.log("YearScales.activate [40]",songs);
+    if (!songs) return;
+
+    let decades = {};
+    let years = {};
 
     for (let decade = 1950; decade <= 2010; decade += 10) {
       let title = (""+decade)+"s"
-      this.decades[decade] = {title:title, songCount:0, score:0};
+      decades[decade] = {title:title, count:0, score:0};
     }
 
     for (let year = 1950; year <= 2017; year++) {
       let title = (""+year).substr(2,1)+" "+(""+year).substr(3,1);
-      this.years[year] = {title:title, songCount:0, score:0};
+      years[year] = {title:title, count:0, score:0};
     }
 
     for (let songSlug in (songs || {})) {
@@ -35,57 +60,18 @@ export class YearScales {
       }
       let decade = song.debutEra.decade;
       if (decade) {
-        this.decades[decade].songCount++;
-        this.decades[decade].score += song.score;
+        decades[decade].count++;
+        decades[decade].score += song.score;
       }
       let year = song.debutEra.year;
       if (year) {
-        this.years[year].songCount++;
-        this.years[year].score += song.score;
+        years[year].count++;
+        years[year].score += song.score;
       }
     }
 
-    let maxCount = 1; // ensure that divisor is always greater than 0
-    let maxAA = 0.01;
-
-    Object.keys(this.decades).forEach(decadeNumber => {
-      let decade = this.decades[decadeNumber];
-      decade.songAdjustedAverage = decade.score / Math.sqrt(decade.songCount);
-      if (decade.songCount > maxCount) maxCount = decade.songCount;
-      if (decade.songAdjustedAverage > maxAA) maxAA = decade.songAdjustedAverage;
-    });
-
-    Object.keys(this.decades).forEach(decadeNumber => {
-      let decade = this.decades[decadeNumber];
-      let r = (decade.songAdjustedAverage/maxAA)/(decade.songCount/maxCount);
-      decade.highlight = "";
-      if (r >= 1.2) {
-        decade.highlight = "leader";
-      } else if (r <= 0.8) {
-        decade.highlight = "lagger";
-      }
-    });
-
-    maxCount = 1; // ensure that divisor is always greater than 0
-    maxAA = 0.01;
-
-    Object.keys(this.years).forEach(yearNumber => {
-      let year = this.years[yearNumber];
-      year.songAdjustedAverage = year.score / Math.sqrt(year.songCount);
-      if (year.songCount > maxCount) maxCount = year.songCount;
-      if (year.songAdjustedAverage > maxAA) maxAA = year.songAdjustedAverage;
-    });
-
-    Object.keys(this.years).forEach(yearNumber => {
-      let year = this.years[yearNumber];
-      let r = (year.songAdjustedAverage/maxAA)/(year.songCount/maxCount);
-      year.highlight = "";
-      if (r >= 1.2) {
-        year.highlight = "leader";
-      } else if (r <= 0.8) {
-        year.highlight = "lagger";
-      }
-    });
+    this.decades = this._aggregateEra(decades);
+    this.years = this._aggregateEra(years);
 
   }
 
