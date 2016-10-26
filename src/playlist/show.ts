ec4c30673bec471c111b80efb73b210dce29c63d
Jay Bienvenu <github@bienv.com>
2016-10-26 12:42:42 -0500

    Implement playlist/show.

---
+++ src/playlist/show.ts
@@ -0,0 +1,28 @@
+import {Data} from '../resources/data';
+
+export class Playlist extends Data {
+
+  fetchRouteFn = (parameters) => 'playlists/compiled/'+parameters.slug;
+
+  title = '(Playlist)';
+  description = "";
+
+  songs = {};
+  songListModel = {};
+  songCount = 0;
+
+  showOnly = [];
+  sort: string;
+
+  massage(inbound) {
+    this.description = inbound.description;
+
+    this.songs = inbound.songs;
+    this.songCount = Object.keys(inbound.songs).length;
+
+    this.showOnly = inbound.columns || ['rank','title','artist','score','debutDate','duration','ascent','descent'];
+    this.sort = inbound.sort || "score";
+
+  }
+
+}
