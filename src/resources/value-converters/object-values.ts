c279fcffb8e747c5398edb7f3df742b04cdf9a8f
Jay Bienvenu <github@bienv.com>
2016-10-26 12:27:23 -0500

    Account for string and array inputs.

--- src/resources/value-converters/object-values.ts
+++ src/resources/value-converters/object-values.ts
@@ -1,10 +1,14 @@
 // A ValueConverter for iterating an Object's values inside of a repeat.for in Aurelia.
 export class ObjectValuesValueConverter {
     toView(obj) {
+        if (Array.isArray(obj)) return obj;
         let temp = [];
+        if (/number|string|boolean/.test(obj)) {
+          temp.push(obj);
+          return temp;
+        }
         for (let key in obj) {
           let value = obj[key];
-          value.__key = key;
           temp.push(value);
         }
         return temp;
