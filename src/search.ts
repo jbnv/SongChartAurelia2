443cfa05c419db95e6e6a4bb8b6ec2441e9652c5
Jay Bienvenu <github@bienv.com>
2016-10-30 20:56:30 -0500

    Search: Bring in summarized data; filter locally.

--- src/search.ts
+++ src/search.ts
@@ -1,12 +1,26 @@
 import {Collection} from './resources/collection';
+import {History} from './resources/history';
 
 export class Search extends Collection {
-  fetchRouteFn = (parameters) => 'search/terms/'+parameters.slug;
 
   activate(parameters,routeConfig) {
     this.title = parameters.slug;
     super.activate(parameters,routeConfig);
-    this.sortByScore();
+
+    let expression = new RegExp(parameters.slug,"gi");
+    let filterFn = item => expression.test(item.title);
+    this.sort("score");
+
+    let query = firebase.database().ref("search/entities");
+    query.on('value', (snapshot) => {
+      let inbound = this._valueFromSnapshot(snapshot);
+      this.items = {};
+      for (let itemSlug in inbound) {
+        let item = inbound[itemSlug];
+        if (filterFn(item)) this.items[itemSlug] = item;
+      }
+    });
+    (new History()).mark("search",{title:parameters.slug,type:"search"}).then(); //INCOMPLETE
   }
-  
+
 }
