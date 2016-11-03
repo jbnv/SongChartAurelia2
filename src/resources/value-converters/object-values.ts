54e4f68f7a54370e1cf982761bab7a326d741a16
Jay Bienvenu <github@bienv.com>
2016-11-03 13:59:30 -0500

    Add options.

--- src/resources/value-converters/object-values.ts
+++ src/resources/value-converters/object-values.ts
@@ -1,6 +1,7 @@
 // A ValueConverter for iterating an Object's values inside of a repeat.for in Aurelia.
 export class ObjectValuesValueConverter {
-    toView(obj) {
+    toView(obj,options) {
+        if (!options) options = {};
         if (Array.isArray(obj)) return obj;
         let temp = [];
         if (/number|string|boolean/.test(typeof obj)) {
@@ -9,6 +10,7 @@ export class ObjectValuesValueConverter {
         }
         for (let key in obj) {
           let value = obj[key];
+          if (options.kvp) value = {__key: key, __value:obj[key]};
           temp.push(value);
         }
         return temp;
