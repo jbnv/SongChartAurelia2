acc17ae59bebdc4126643ac8e4c3c0e756b06742
Jay Bienvenu <github@bienv.com>
2016-11-02 19:12:51 -0500

    Fix rank bars and other fixes.

--- src/artist/show.ts
+++ src/artist/show.ts
@@ -14,6 +14,7 @@ export class Artist extends Data {
   death = "";
   origin = "";
   songs = {};
+  rank: number;
   score = 0.00;
   songAdjustedAverage = 0.00;
   status = "";
@@ -30,7 +31,7 @@ export class Artist extends Data {
   collaboratorShowOnly = [ 'title','songCount','score' ];
 
   massage(inbound) {
-    console.log("inbound",inbound);
+    this.rank = inbound.__rank;
     this.type = inbound.type.slug;
     this.typeTitle = inbound.type.title || "NOT SET";
     this.status
@@ -40,12 +41,12 @@ export class Artist extends Data {
     this.members = inbound.members || {};
     this.xref = inbound.xref || {};
     this.tags = inbound.tags || {};
+    this.ranks = inbound.ranks || {};
 
     this.score = numeral(inbound.score || 0).format("0.00");
     this.songAdjustedAverage = numeral(inbound.songAdjustedAverage || 0).format("0.00");
 
     // Pull out ranks and add them to their respective entities.
-    // (Could this maybe go on the server side?)
     Object.keys(this.ranks || {}).forEach(key => {
       let rank = this.ranks[key];
       let keyParts = key.split(":");
@@ -66,7 +67,7 @@ export class Artist extends Data {
           entity = this.tags[instanceSlug];
           break;
       }
-      if (entity) {
+      if (typeof entity == "object") {
         entity.rank = rank.rank;
         entity.count = rank.total;
       }
