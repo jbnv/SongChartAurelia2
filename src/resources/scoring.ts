aba3779f8adf2bf9e4aa0cc73bf5c4d0187efca6
Jay Bienvenu <github@bienv.com>
2016-11-06 19:03:37 -0600

    Write bent scores to compiled.

--- src/resources/scoring.ts
+++ src/resources/scoring.ts
@@ -1,6 +1,21 @@
 import 'firebase';
 import {Configuration} from './firebase/index';
 
+function _score():any {
+
+  var peak = arguments[0] || 0,
+      ascentWeeks = arguments[1] || 0,
+      descentWeeks = arguments[2] || 0;
+
+  if (typeof arguments[0] == "object") {
+    peak = arguments[0].peak || 0;
+    ascentWeeks = arguments[0]["ascent-weeks"] || 0;
+    descentWeeks = arguments[0]["descent-weeks"] || 0;
+  }
+
+  return (2/3) * peak * (ascentWeeks+descentWeeks);
+}
+
 function _bend(c) {
   if (c == 0) return function(x) { return x };
   if (c > 0) return function(x) {
@@ -13,22 +28,39 @@ function _bend(c) {
   }
 }
 
-function _query(songSlug) {
+function _queryRaw(songSlug) {
   return firebase.database().ref("songs/raw").child(songSlug);
 }
 
-export function bendUp(songSlug) {
-  let query = _query(songSlug).child("peak");
-  query.once("value").then(snapshot => {
-    let value = _bend(1)(snapshot.val() || "0.5");
-    query.set(value);
-  });
+function _queryCompiled(songSlug) {
+  return firebase.database().ref("songs/compiled").child(songSlug);
 }
 
-export function bendDown(songSlug) {
-  let query = _query(songSlug).child("peak");
-  query.once("value").then(snapshot => {
-    let value = _bend(-1)(snapshot.val() || "0.5");
-    query.set(value);
-  });
+function _write(slug,song) {
+
+  let queryRaw = firebase.database().ref("songs/raw").child(slug);
+  queryRaw.child("peak").set(song.peak);
+  queryRaw.child("ascent-weeks").set(song["ascent-weeks"]);
+  queryRaw.child("descent-weeks").set(song["descent-weeks"]);
+
+  let queryCompiled = firebase.database().ref("songs/compiled").child(slug);
+  queryCompiled.child("peak").set(song.peak);
+  queryCompiled.child("ascent-weeks").set(song["ascent-weeks"]);
+  queryCompiled.child("descent-weeks").set(song["descent-weeks"]);
+  queryCompiled.child("score").set(_score(song));
+
 }
+
+function bendFn(coefficient) {
+  return function(songSlug) {
+    let query = _queryRaw(songSlug);
+    query.once("value").then(snapshot => {
+      let song = snapshot.val();
+      song.peak = _bend(coefficient)(song.peak || "0.5");
+      _write(songSlug,song);
+    });
+  }
+}
+
+export const bendUp = bendFn(1);
+export const bendDown = bendFn(-1);
