1d4040547a1a4b554e1c378d7a519d75ed223904
Jay Bienvenu <github@bienv.com>
2016-11-12 14:12:21 -0600

    Implement decade view. (WIP)

---
+++ src/era/decade.ts
@@ -0,0 +1,59 @@
+import {Data} from '../resources/data';
+//import * as gregoria from 'gregoria';
+
+export class Decade extends Data {
+
+  fetchRouteFn = (parameters) => 'decades/'+parameters.slug; // returns array of songs
+  type = "decade";
+
+  score: number;
+  count: number;
+
+  songs = {};
+  years = [];
+  year0 = 0;
+
+  previous = {slug: null, route: null, title: null};
+  next = {slug: null, route: null, title: null};
+  pages = [];
+
+  countScales = [];
+  aaScales = [];
+
+  massage(inbound) {
+    console.log("[22]",inbound); //TEMP
+    this.title = this.slug;
+    this.songs = inbound.songs || {};
+    this.count = inbound.count;
+    this.score = inbound.score;
+    this.year0 = parseInt(this.slug.substring(0,4));
+    this.years = [0,1,2,3,4,5,6,7,8,9].map(n => this.year0+n);
+
+    this.previous.slug = `${this.year0-10}s`;
+    this.previous.route = `decade/${this.previous.slug}`;
+    this.previous.title = this.previous.slug
+
+    this.next.slug = `${this.year0+10}s`;
+    this.next.route = `decade/${this.next.slug}`;
+    this.next.title = this.next.slug;
+
+    this.pages = this.years.map(function(y) { return { title: y, route: "year/"+y };})
+
+    // this.countScales = [];
+    // this.aaScales = [];
+    // this.years.forEach(year => {
+    //   this.countScales.push({
+    //     title: year.title,
+    //     scale: year.songCountScale,
+    //     route: "year/"+year.slug
+    //   });
+    //   this.aaScales.push({
+    //     title: year.title,
+    //     scale: year.songAdjustedAverageScale,
+    //     route: "year/"+year.slug
+    //   });
+    // });
+
+  }
+
+}
