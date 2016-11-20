28fd76fa466ecb664fee218cdff8aff776894c10
Jay Bienvenu <github@bienv.com>
2016-11-20 15:43:47 -0600

    Remove event aggregator (already part of ReactiveCollection).

--- src/resources/data.ts
+++ src/resources/data.ts
@@ -1,10 +1,6 @@
-import {inject} from 'aurelia-framework';
-import {EventAggregator} from 'aurelia-event-aggregator'; // jspm install aurelia-event-aggregator
-
 import {ReactiveCollection} from "./firebase/collection"
 import {History} from '../resources/history';
 
-@inject(EventAggregator)
 export class Data extends ReactiveCollection {
 
   title = "";
@@ -19,12 +15,6 @@ export class Data extends ReactiveCollection {
   navModel = null;
 
   subscription: any;
-  eventAggregator: any;
-
-  constructor(eventAggregator) {
-    super();
-    this.eventAggregator = eventAggregator;
-  }
 
   _massage(inbound) {
     console.log("Data._massage [18]"); //TEMP
@@ -61,14 +51,6 @@ export class Data extends ReactiveCollection {
     this.route = routeConfig.route;
     this.navModel = routeConfig.navModel;
     return this.refresh();
-  }
-
-  bind() {
-    this.subscription = this.eventAggregator.subscribe('reload',this.refresh);
-  }
-
-  unbind() {
-    this.subscription.dispose();
   }
 
 }
