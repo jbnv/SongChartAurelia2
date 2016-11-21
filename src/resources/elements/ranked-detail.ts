f5e99ac840ac85635287ccf1d1325eaf628dc4d7
Jay Bienvenu <github@bienv.com>
2016-11-20 19:09:42 -0600

    Add all items and editable mode. (WIP)

--- src/resources/elements/ranked-detail.ts
+++ src/resources/elements/ranked-detail.ts
@@ -3,8 +3,10 @@ import {bindable} from 'aurelia-framework';
 export class RankedDetail {
 
   @bindable items = {};
+  @bindable allItems = {};
   @bindable ranks = {};
   @bindable typeSlug:string;
+  @bindable editable:false;
 
   rankItem(item) {
     return this.ranks[`${this.typeSlug}:${item.__key}`];
